41e58cca527cf434c16e48044cfa03c08f21b83d
Maro <maroegy.dev@gmail.com>
2025-10-04 17:52:03 +0200

    feat: implemented the hero's interactive background

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+VANTA.GLOBE({
+  el: "#hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x6c63ff,
+  color2: 0x00fff5,
+  backgroundColor: 0x0b0e16,
+});
